be8ccb2915ed91367b93b8909d508b0a19d72264
DESKTOP-2GK6IL7\me <v2pok@naver.com>
2022-03-07 21:20:55 +0900

    불필요코드 삭제 및 여기어때 잘난체 폰트 적용

--- src/pages/productPage/index.vue
+++ src/pages/productPage/index.vue
@@ -1,12 +1,21 @@
 <template>
-  <div class="product">
-    <div>product</div>
+  <div class="product-page">
+    여기어때
+    <div class="product-header">
+      수정어때
+    </div>
   </div>
 </template>
 
 <script>
+import HeaderComponent from '@/components/headerComponent'
+
 export default {
   name: 'ProductPage',
+  components: {
+    // eslint-disable-next-line vue/no-unused-components
+    HeaderComponent,
+  },
   data() {
     return {
       checkBoxData: '',
@@ -16,6 +25,16 @@ export default {
 }
 </script>
 
-<style scoped>
+<style scoped lang="scss">
+.product-page {
+  padding: 5px;
+  background-color: #42b983;
+  .product-header {
+    padding: 5px;
+    background-color: bisque;
+
+    font-family: Jalnan;
+  }
+}
 
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -6,14 +6,6 @@ Vue.use(VueRouter)
 
 const routes = [
   { path: '/', name: 'ProductPage', component: ProductPage },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
 ]
 
 const router = new VueRouter({
