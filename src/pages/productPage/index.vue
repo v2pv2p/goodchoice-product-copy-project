4e999bc780e5540b32bf40689105f53a77fefec2
DESKTOP-2GK6IL7\me <v2pok@naver.com>
2022-03-07 17:05:58 +0900

    first commit

---
+++ src/pages/productPage/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="product">
+    <div>product</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ProductPage',
+  data() {
+    return {
+      checkBoxData: '',
+    }
+  },
+  methods: {}
+}
+</script>
+
+<style scoped>
+
+</style>
