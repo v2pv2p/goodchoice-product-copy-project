e94090f61f70195500f17f1a7a861b3d1575d9a6
DESKTOP-2GK6IL7\me <v2pok@naver.com>
2022-03-09 00:46:00 +0900

    sub-header 추가

--- src/pages/productPage/index.vue
+++ src/pages/productPage/index.vue
@@ -2,8 +2,21 @@
   <!--  pc 모바일 여부 class 추가하기-->
   <div class="product-page">
     <!--    스크롤 여부 class 추가하기-->
-    <header-component class="header" :isScroll="isScroll"></header-component>
-    <div class="product-content">123데이터데이터데이터데이터데이터데이터데이터데이터데이터데이터데이터데이터데이터데이터데이터데이터데이터데이터데이터데이터데이터데이터데이터데이터데이터데이터데이터데이터데이터데이터데이터데이터데이터데이터데이터데이터데이터데이터데이터데이터데이터데이터데이터데이터데이터데이터데이터데이터데이터데이터데이터데이터데이터데이터데이터데이터데이터데이터데이터데이터데이터데이터데이터데이터데이터데이터데이터데이터데이터데이터데이터데이터데이터데이터</div>
+    <header-component :isScroll="isScroll"></header-component>
+
+    <div class="sub-header">
+      <div class="sub-header-wrapper">
+        <div class="sub-header-title">내주변</div>
+        <div class="location-area">
+          <div class="my-location">중구 태평로1가</div>
+          <div class="location-reset">내 위치 재설정</div>
+        </div>
+      </div>
+    </div>
+
+    <div class="product-content">
+      123데이터데이터데이터데이터데이터데이터데이터데이터데이터데이터데이터데이터데이터데이터데이터데이터데이터데이터데이터데이터데이터데이터데이터데이터데이터데이터데이터데이터데이터데이터데이터데이터데이터데이터데이터데이터데이터데이터데이터데이터데이터데이터데이터데이터데이터데이터데이터데이터데이터데이터데이터데이터데이터데이터데이터데이터데이터데이터데이터데이터데이터데이터데이터데이터데이터데이터데이터데이터데이터데이터데이터데이터데이터데이터
+    </div>
   </div>
 </template>
 
@@ -16,8 +29,7 @@ export default {
     // eslint-disable-next-line vue/no-unused-components
     HeaderComponent,
   },
-  computed: {
-  },
+  computed: {},
   data() {
     return {
       isScroll: false,
@@ -43,6 +55,59 @@ export default {
   height: 100%;
 
   background-color: #42b983;
+
+  .sub-header {
+    background-color: #f7323f;
+
+    .sub-header-wrapper {
+      width: 1024px;
+      height: 139px;
+
+      padding-top: 72px;
+      margin: 0 auto;
+
+      .sub-header-title {
+        margin-top: 21px;
+
+        font-size: 38px;
+        font-weight: normal;
+        color: #ffffff;
+      }
+
+      .location-area {
+        display: flex;
+        margin-top: 15px;
+
+        .my-location {
+          margin: 0 10px 0 0;
+          font-size: 18px;
+          font-weight: bold;
+          line-height: 32px;
+          color: #ffffff;
+        }
+
+        .location-reset {
+          height: 32px;
+          padding: 0 6px;
+
+          border: 1px solid rgba(0, 0, 0, 0.04);
+          border-radius: 4px;
+
+          background: rgba(0, 0, 0, 0.04);
+
+          font-size: 14px;
+          line-height: 32px;
+
+          color: #fff;
+
+          &:hover {
+            background: rgba(0, 0, 0, 0.08);
+            border: 1px solid rgba(0, 0, 0, 0.08);
+          }
+        }
+      }
+    }
+  }
 
   .product-content {
     width: 1024px;
